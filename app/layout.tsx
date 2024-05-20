88a8c0667b84c8573a53e6e0263d7555fff63da7
awaik <awaiking44@gmail.com>
2024-05-20 14:37:24 +0300

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-
-export default function RootLayout({
+//s
+export default function RootLayout({  
   children,
 }: {
   children: React.ReactNode;
