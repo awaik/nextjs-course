d5eb93a44300bc92643facaa1583b8717faaa009
awaik <awaiking44@gmail.com>
2024-05-20 14:33:28 +0300

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+"use client;"
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
@@ -9,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
